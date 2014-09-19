d5ef549246729fe542d511d72b5956a9c27c67bc
Gaya <gaya.kessler@gmail.com>
2014-09-19 21:10:40 +0200

    debounce the scroll method

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ var headerElement = document.querySelector("header.main-header");
 var navElement = document.querySelector("nav.main-nav");
 var fixed = false;
 
-window.onscroll = function () {
+window.onscroll = debounce(function () {
     "use strict";
     var headerSize = headerElement.getBoundingClientRect();
     var top = (window.pageYOffset || document.documentElement.scrollTop)  - (document.documentElement.clientTop || 0);
@@ -17,4 +17,20 @@ window.onscroll = function () {
         headerElement.style.marginBottom = "0px";
         fixed = false;
     }
+}, 10);
+
+function debounce(func, wait, immediate) {
+    "use strict";
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
 };
