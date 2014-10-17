eb25c3789504e4f5f026b1e63afc3a1c53c8ce12
Gaya <gaya.kessler@gmail.com>
2014-10-17 21:00:43 +0200

    reload the stream

--- gulpfile.js
+++ gulpfile.js
@@ -189,7 +189,7 @@ gulp.task('wintersmith', function () {
 
     env.build(function(error) {
         if (error) { throw error; }
-        browserSync.reload({stream:true});
+        browserSync.reload();
     });
 });
 
