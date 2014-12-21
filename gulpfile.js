5e299ee9369e166b8040e8c6251f7fb9d04a24d1
Gaya <gaya.kessler@gmail.com>
2014-12-21 20:57:00 +0100

    put in base code

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,34 @@
 var gulp = require('gulp'),
+    fs = require('fs');
+
+gulp.browserSync = require('browser-sync');
+gulp.config = require('./gulpconfig.js');
+gulp.pkg = require('./package.json');
+
+//read all tasks
+var files = fs.readdirSync("./" + gulp.config.gulp);
+files.forEach(function (file) {
+    "use strict";
+    if (file.substr(-3) !== ".js") {
+        return;
+    }
+
+    var tasks = require("./" + gulp.config.gulp + "/" + file);
+
+    if (tasks.length === undefined) {
+        tasks = [tasks];
+    }
+
+    tasks.forEach(function (task) {
+        if (task.pre) {
+            gulp.task(task.name, task.pre, task.task);
+        } else {
+            gulp.task(task.name, task.task);
+        }
+    });
+});
+
+var gulp = require('gulp'),
     source = require('vinyl-source-stream'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync'),
