0d326bfcaca3dc143126936b0d7681beb0f795c5
Gaya <gaya.kessler@gmail.com>
2015-01-26 19:30:41 +0100

    contact form query

--- src/js/contactform/index.js
+++ src/js/contactform/index.js
@@ -1,5 +1,12 @@
 var contactform = {
+    init: function (query) {
+        "use strict";
+        var objects = document.querySelectorAll(query);
 
+        for (var i = 0; i < objects.length; i++) {
+            objects[i];
+        }
+    }
 };
 
 module.exports = function createContactForm () {
--- src/js/inlineRhythm/index.js
+++ src/js/inlineRhythm/index.js
@@ -4,6 +4,7 @@ var inlineRhythm = {
     lineHeight: 30,
 
     init: function(query) {
+        "use strict";
         var objects = document.querySelectorAll(query);
 
         for (var i = 0; i < objects.length; i++) {
