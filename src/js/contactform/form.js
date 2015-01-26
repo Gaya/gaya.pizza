50e6993e3d96c75ddfe1bf227f7759900edcf3b0
Gaya <gaya.kessler@gmail.com>
2015-01-26 19:37:07 +0100

    catch submit

--- src/js/contactform/form.js
+++ src/js/contactform/form.js
@@ -1,7 +1,36 @@
+var _ = require("underscore");
+
 var formObject = {
     init: function (form) {
         "use strict";
+        this.element = form;
 
+        this.bindSubmit();
+    },
+
+    bindEvent: function (target, event, action) {
+        "use strict";
+        if(target.addEventListener) {
+            target.addEventListener(event, action, false);
+        } else {
+            target.attachEvent('on' + event, action);
+        }
+    },
+
+    bindSubmit: function () {
+        "use strict";
+        this.bindEvent(this.element, 'submit', _.bind(function (e) {
+            e.preventDefault();
+
+            if (this.validateFields()) {
+                this.element.submit();
+            }
+        }, this));
+    },
+
+    validateFields: function () {
+        "use strict";
+        return false;
     }
 };
 
