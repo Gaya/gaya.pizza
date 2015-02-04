70d19262f6bd1e4b0db89e1663ffddb840b4235c
Gaya <gaya.kessler@gmail.com>
2015-02-04 12:38:42 +0100

    rewrite server to express

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
-var connect = require('connect');
+var app = require('express')();
 var mail = require("./mail.js");
+var bodyParser = require('body-parser');
+var serveStatic = require('serve-static');
 
-connect().use(connect.bodyParser()).use(
-    '/contact', mail
-).use(connect.static(__dirname + "../public")).listen(process.env.PORT || 5000);
+//parse form data
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/contact', function (req, res) {
+    "use strict";
+    console.log(req.body);
+    res.send('welcome');
+});
+
+app.use(serveStatic('../public/', {'index': ['index.html']}));
+
+app.listen(process.env.PORT || 5000);
