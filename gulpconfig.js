d98893bc10725aa868276f742509cdfa754e3e31
Gaya <gaya.kessler@gmail.com>
2015-02-04 13:25:00 +0100

    add build folder to public

--- gulpconfig.js
+++ gulpconfig.js
@@ -2,7 +2,7 @@ module.exports = {
     "gulp": "gulp",
     "src": 'src',
     "dist": 'dist',
-    "build": 'build',
+    "build": 'public',
     "blog": 'blog',
     "port": 3000,
     "middlewares": [function handleMiddleWare (req, res, next) {
