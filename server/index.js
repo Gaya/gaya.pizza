bea1db944fcd4df3d045fc74a72e519944461c61
Gaya <gaya.kessler@gmail.com>
2015-02-04 12:12:42 +0100

    target correct folder

--- server/index.js
+++ server/index.js
@@ -3,4 +3,4 @@ var mail = require("./mail.js");
 
 connect().use(connect.bodyParser()).use(
     '/contact', mail
-).use(connect.static(__dirname)).listen(process.env.PORT || 5000);
+).use(connect.static(__dirname + "../public")).listen(process.env.PORT || 5000);
