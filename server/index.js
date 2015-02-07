5e612e5bc617ac125256d8928585c5397cc9054c
Gaya <gaya.kessler@gmail.com>
2015-02-07 16:21:49 +0100

    add expires headers

--- server/index.js
+++ server/index.js
@@ -13,7 +13,11 @@ app.use(compression());
 
 app.use(serveStatic(__dirname + '/../public/', {
     'index': ['index.html'],
-    'maxAge': (86400000 * 365)
+    'maxAge': (86400000 * 365),
+    'setHeaders': function setHeaders(req, res, next) {
+        "use strict";
+        res.setHeader('Expires', new Date(Date.now() + 345600000).toUTCString());
+    }
 }));
 
 app.listen(process.env.PORT || 5000);
