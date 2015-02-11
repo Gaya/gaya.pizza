ecf33dd0957225c67fdc87aebb1ed0509b27a72b
Gaya <gaya.kessler@gmail.com>
2015-02-11 14:30:24 +0100

    add 404 response

--- server/index.js
+++ server/index.js
@@ -7,10 +7,13 @@ var app = require('express')(),
 //parse form data
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//contact form
 app.post('/contact', mail);
 
+//compress output
 app.use(compression());
 
+//serve static files
 app.use(serveStatic(__dirname + '/../public/', {
     'index': ['index.html'],
     'maxAge': (86400000 * 365),
@@ -19,5 +22,22 @@ app.use(serveStatic(__dirname + '/../public/', {
         res.setHeader('Expires', new Date(Date.now() + 345600000).toUTCString());
     }
 }));
+
+//handle not found
+app.use(function(req, res, next){
+    res.status(404);
+
+    if (req.accepts('html')) {
+        res.render('404', { url: req.url });
+        return;
+    }
+
+    if (req.accepts('json')) {
+        res.send({ error: 'Not found' });
+        return;
+    }
+
+    res.type('txt').send('Not found');
+});
 
 app.listen(process.env.PORT || 5000);
