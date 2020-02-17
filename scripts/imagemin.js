c3ea4892e732a1f522e2b4d8ed74f2d507a7a28f
Gaya Kessler <gaya@theclevernode.com>
2020-02-17 19:09:04 +0100

    solve image steps

---
+++ scripts/imagemin.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const glob = require('glob');
+
+const imagemin = require('imagemin');
+const imageminJpegtran = require('imagemin-jpegtran');
+const imageminPngquant = require('imagemin-pngquant');
+const imageminGifsicle = require('imagemin-gifsicle');
+const imageminSvgo = require('imagemin-svgo');
+
+glob('./public/**/*{.jpg,.jpeg,.png,.gif,.svg}', (err, files) => {
+  if (err) throw err;
+
+  const start = +new Date();
+
+  const optimises = files.map(
+    (file) =>
+      imagemin([file], {
+        destination: path.dirname(file),
+        plugins: [
+          imageminJpegtran(),
+          imageminPngquant({
+            quality: [0.6, 0.8]
+          }),
+          imageminGifsicle(),
+          imageminSvgo(),
+        ]
+      })
+        .then(([file]) => {
+          console.log(`Optimised ${file.sourcePath}`);
+        })
+        .catch(console.error)
+  );
+
+  Promise.all(optimises)
+    .then(() => {
+      console.log(`Optimised images in ${+new Date() - start}ms`);
+    })
+    .catch(console.error);
+});
