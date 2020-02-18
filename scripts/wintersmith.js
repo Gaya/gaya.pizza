fd6c6ee280e8d64be4df09a52a1c5ae9b7d26f17
Gaya Kessler <gaya@theclevernode.com>
2020-02-18 07:46:48 +0100

    add absolute paths

--- scripts/wintersmith.js
+++ scripts/wintersmith.js
@@ -1,3 +1,4 @@
+const replace = require('replace-in-file');
 const wintersmith = require('wintersmith');
 
 const env = wintersmith('./config.json');
@@ -9,4 +10,17 @@ if (process.env.URL) {
 env.build((error) => {
   if (error) throw error;
   console.log('Generated site HTML');
+
+  // place absolute paths
+  if (process.env.URL) {
+    const options = {
+      files: './public/**/*.html',
+      from: /(?:src|href)="(\/)/g,
+      to: (match) => match.replace('\/', `${process.env.URL}/`),
+    };
+
+    replace.sync(options);
+
+    console.log('Replaced URL paths');
+  }
 });
